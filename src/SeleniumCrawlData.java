846e9aed269054a4f077555658da44b82248c14e
justaboy-dev <74774683+justaboy-dev@users.noreply.github.com>
2021-10-16 16:33:04 +0700

    fix-bug-and-storge-json-file

--- src/SeleniumCrawlData.java
+++ src/SeleniumCrawlData.java
@@ -1,5 +1,6 @@
 
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
@@ -7,6 +8,7 @@ import java.util.ArrayList;
 import org.json.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -84,63 +86,102 @@ public class SeleniumCrawlData {
         return returnStr;
     }
     
+    public static JSONArray convertToJson(ArrayList<FoodObject> arr) throws Exception
+    {
+        JSONArray json = new JSONArray();
+        
+        for(int i = 0; i < arr.size(); i++)
+        {
+            JSONObject obj = new JSONObject();
+            JSONObject objitem = new JSONObject();
+            objitem.put("id", String.valueOf(i));
+            objitem.put("name", arr.get(i).name);
+            objitem.put("description", arr.get(i).description);
+            objitem.put("ingredients", arr.get(i).ingredients);
+            objitem.put("preprocessing", arr.get(i).preprocessing);
+            objitem.put("perform", arr.get(i).perform);
+            objitem.put("eating", arr.get(i).eating);
+            objitem.put("tip", arr.get(i).tip);
+            objitem.put("levelOfDifficult", arr.get(i).levelOfDifficult);
+            objitem.put("imageUrl", arr.get(i).imageUrl);
+            obj.put("Food", objitem);
+            json.put(obj);
+        }
+        
+        return json;
+    }
     public static void main(String[] args) throws Exception {
         ArrayList<FoodObject> arrList = new ArrayList<FoodObject>();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        ChromeOptions op = new ChromeOptions();
+        op.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(op);
         int pages = 1;
         boolean hasNextPage = true;
+        //int test = 1; //this varible for testing.
         do{
             try {
                 System.out.println("Go to page: " + pages + "......");
                 driver.get(String.format("https://monngonmoingay.com/tim-kiem-mon-ngon/page/%d/", pages));
-                Thread.sleep(3000);
+                Thread.sleep(2000);
+                ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+                Thread.sleep(4000);
                 ArrayList<WebElement> listElement = (ArrayList<WebElement>)driver.findElements(By.className("col-sm-3"));
-                Thread.sleep(1000);
+                Thread.sleep(2000);
                 for (WebElement webElement : listElement) {
                     FoodObject foodObject = new FoodObject();
                     String src = webElement.findElement(By.tagName("img")).getAttribute("src");
                     foodObject.name = webElement.findElement(By.className("info-list")).findElement(By.tagName("h3")).getText();
+                    System.out.println("Getting info food: " + foodObject.name + ".....");
                     System.out.println("Saving image......");
                     saveImage(src, "images");
-                    String url = webElement.findElement(By.tagName("a")).getAttribute("href");
+                    String url = webElement.findElement(By.className("info-list")).findElement(By.tagName("a")).getAttribute("href");
                     ((JavascriptExecutor)driver).executeScript("window.open()");
-                    ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+                    ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
                     driver.switchTo().window(tabs.get(1));
                     driver.get(url);
-                    Thread.sleep(3000);
+                    Thread.sleep(2000);
+                    ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+                    Thread.sleep(4000);
                     //something to do
                     
-                    System.out.println("Getting info food:" + foodObject.name + ".....");
                     foodObject.imageUrl = ("images/"+getname(src));
-                    
                     foodObject.description = driver.findElement(By.cssSelector("[class='pt-2 pb-4']")).getAttribute("innerText");
                     ArrayList<WebElement> rows = (ArrayList<WebElement>) driver.findElements(By.cssSelector("[class='row mb-3']"));
-                    
                     ArrayList<WebElement> temp = (ArrayList<WebElement>)driver.findElements(By.cssSelector("[class='nav justify-content-center']"));
-                    foodObject.levelOfDifficult = temp.get(0).getAttribute("innerText");
+                    String diff = temp.get(0).getAttribute("innerText");
+                    foodObject.levelOfDifficult = diff.substring(0,diff.indexOf("\n"));
                     foodObject.ingredients = getContent(rows, 0);
                     foodObject.preprocessing = getContent(rows, 1);
                     foodObject.perform = getContent(rows, 2);
                     foodObject.eating = getContent(rows, 3);
                     foodObject.tip = getContent(rows, 4);
                     arrList.add(foodObject);
+                    System.out.println("Getting success: " + foodObject.name);
+                    Thread.sleep(2000);
                     
-                    
-                    System.out.println("Getting success: " + foodObject.name + ".....");
-                    
-                    Thread.sleep(1000);
                     ((JavascriptExecutor)driver).executeScript("window.close()");
+                    
+                    
+                    Thread.sleep(2000);
                     driver.switchTo().window(tabs.get(0));
                 }
                 pages++;
+                //test++; testing
             } catch (Exception e) {
                 System.err.println("Error !!!!!!!!!!!!!!");
                 e.printStackTrace();
                 hasNextPage = false;
             }
-        }while (hasNextPage);
-       JSONArray Jsonarr = new JSONArray();
+        }
+        while (hasNextPage);
+        //while (hasNextPage && test <1); //this while for testing
+        JSONArray Jsonarr = convertToJson(arrList);
+        try (FileWriter file = new FileWriter("output/data.json")){
+            System.out.println("Saving JSON file.......");
+            file.write(Jsonarr.toString());
+            System.out.println("Saving file successfull !");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
 }
